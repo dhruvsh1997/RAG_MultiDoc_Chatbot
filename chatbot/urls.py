8587036920f592f3e9d66c12ccbf17ea4a4d8a51
dhruvsh1997 <dhruvsh1997@gmail.com>
2025-03-29 23:25:01 +0530

    Update URLs

--- chatbot/urls.py
+++ chatbot/urls.py
@@ -3,7 +3,7 @@ from django.contrib.auth import views as auth_views
 from . import views
 
 urlpatterns = [
-    # Authentication
+    # Authentication Views URL
     path('login/', views.login_view, name='login'),
     path('logout/', auth_views.LogoutView.as_view(), name='logout'),
     path('register/', views.register_view, name='register'),
