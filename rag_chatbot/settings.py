d04c6a4caddb32fa8bf7917f2b38b80b53e648bc
dhruvsh1997 <dhruvsh1997@gmail.com>
2025-04-02 12:39:53 +0530

    Updates Settings.py

--- rag_chatbot/settings.py
+++ rag_chatbot/settings.py
@@ -21,7 +21,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
-# SECURITY WARNING: keep the secret key used in production secret!
+# SECURITY WARNING: keep the secret key used in production secret!!
 SECRET_KEY = 'django-insecure-0!d3+1-l38t0k(fe1cuzd@pf^q-50v!vh@gq$a)k-jes&i$k7)'
 
 # SECURITY WARNING: don't run with debug turned on in production!
